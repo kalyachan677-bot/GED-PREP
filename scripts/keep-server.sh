3e337d8f0074157688ab136883333dfd25ab294f
Z User <z@container>
2026-08-05 08:56:45 +0000

    b550316f-929a-4acc-bca8-b964f6041598

--- scripts/keep-server.sh
+++ scripts/keep-server.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 cd /home/z/my-project
 while true; do
-  echo "[$(date)] Starting server..." >> /tmp/keeper.log
-  npx next start -p 3000 2>&1 | tee -a /tmp/next-err.log
-  echo "[$(date)] Crashed, restart in 2s" >> /tmp/keeper.log
+  npx next dev -p 3000 2>&1 | tee -a /tmp/next-err.log
+  echo "[$(date)] RESTART" >> /tmp/keeper.log
   sleep 2
 done
